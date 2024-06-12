85cc31dee42bc6931bbc351538cf5d590e0e0020
Domirando <maisiedev@gmail.com>
2024-06-12 09:30:21 +0500

    changed

--- app/src/main/java/com/example/myapplication/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/screens/home/HomeViewModel.kt
@@ -28,8 +28,19 @@ class HomeViewModel(val model: HomeModel) : ViewModel() {
         Item(name = "Ishqalanish kuchi!", description = "Gravitatsiya tufayli Yer va boshqa sayyoralar Quyosh atrofida, Oy Yer atrofida aylanadi; sayyora va yulduzlar ichi qiziydi; tabiatda konveksiya sodir boʻladi va hk.\n" +
                 "\n" +
                 "Gravitatsiya toʻrt fundamental oʻzaro taʼsirdan biridir (boshqalari — elektromagnetizm, kuchli oʻzaro taʼsir va kuchsiz oʻzaro taʼsir)."+"\n"+"Savol!\n Nima Nyutonning Gravitatsiya qonuniga asoslangan?"),
-        )
-
+        Item(name = "Gravitatsiya qonuni?!", description = "Gravitatsiya tufayli Yer va boshqa sayyoralar Quyosh atrofida, Oy Yer atrofida aylanadi; sayyora va yulduzlar ichi qiziydi; tabiatda konveksiya sodir boʻladi va hk.\n" +
+        "\n" +
+        "Gravitatsiya toʻrt fundamental oʻzaro taʼsirdan biridir (boshqalari — elektromagnetizm, kuchli oʻzaro taʼsir va kuchsiz oʻzaro taʼsir)."+"\n"+"Savol!\n Nima Nyutonning Gravitatsiya qonuniga asoslangan?"),
+        Item(name = "Ishqalanish kuchi!", description = "Gravitatsiya tufayli Yer va boshqa sayyoralar Quyosh atrofida, Oy Yer atrofida aylanadi; sayyora va yulduzlar ichi qiziydi; tabiatda konveksiya sodir boʻladi va hk.\n" +
+        "\n" +
+        "Gravitatsiya toʻrt fundamental oʻzaro taʼsirdan biridir (boshqalari — elektromagnetizm, kuchli oʻzaro taʼsir va kuchsiz oʻzaro taʼsir)."+"\n"+"Savol!\n Nima Nyutonning Gravitatsiya qonuniga asoslangan?"),
+        Item(name = "Energiya bu nima?!", description = "Gravitatsiya tufayli Yer va boshqa sayyoralar Quyosh atrofida, Oy Yer atrofida aylanadi; sayyora va yulduzlar ichi qiziydi; tabiatda konveksiya sodir boʻladi va hk.\n" +
+        "\n" +
+        "Gravitatsiya toʻrt fundamental oʻzaro taʼsirdan biridir (boshqalari — elektromagnetizm, kuchli oʻzaro taʼsir va kuchsiz oʻzaro taʼsir)."+"\n"+"Savol!\n Nima Nyutonning Gravitatsiya qonuniga asoslangan?"),
+        Item(name = "Kinetik energiya va potensial energiyalar farqi!", description = "Gravitatsiya tufayli Yer va boshqa sayyoralar Quyosh atrofida, Oy Yer atrofida aylanadi; sayyora va yulduzlar ichi qiziydi; tabiatda konveksiya sodir boʻladi va hk.\n" +
+        "\n" +
+        "Gravitatsiya toʻrt fundamental oʻzaro taʼsirdan biridir (boshqalari — elektromagnetizm, kuchli oʻzaro taʼsir va kuchsiz oʻzaro taʼsir)."+"\n"+"Savol!\n Nima Nyutonning Gravitatsiya qonuniga asoslangan?"),
+    )
     init {
         if (model.isFirstRun()) addItems()
         getItems()
